e1c893a145534e2fe1c8352551307be6e4f33520
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2026-03-02 08:55:56 +0100

    fix: use textencoder
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

--- packages/crypto/src/hash.ts
+++ packages/crypto/src/hash.ts
@@ -1,42 +1,19 @@
 import { sha256 as nobleSha256, sha384 as nobleSha384, sha512 as nobleSha512 } from '@noble/hashes/sha2.js'
 
-function toUTF8Array(str: string): Uint8Array {
-  const utf8: Array<number> = []
-  for (let i = 0; i < str.length; i++) {
-    let charcode = str.charCodeAt(i)
-    if (charcode < 0x80) utf8.push(charcode)
-    else if (charcode < 0x800) {
-      utf8.push(0xc0 | (charcode >> 6), 0x80 | (charcode & 0x3f))
-    } else if (charcode < 0xd800 || charcode >= 0xe000) {
-      utf8.push(0xe0 | (charcode >> 12), 0x80 | ((charcode >> 6) & 0x3f), 0x80 | (charcode & 0x3f))
-    }
-    // surrogate pair
-    else {
-      i++
-      charcode = 0x10000 + (((charcode & 0x3ff) << 10) | (str.charCodeAt(i) & 0x3ff))
-      utf8.push(
-        0xf0 | (charcode >> 18),
-        0x80 | ((charcode >> 12) & 0x3f),
-        0x80 | ((charcode >> 6) & 0x3f),
-        0x80 | (charcode & 0x3f)
-      )
-    }
-  }
-  return new Uint8Array(utf8)
-}
+const textEncoder = new TextEncoder()
 
 export const sha256 = (text: string | ArrayBuffer): Uint8Array => {
-  const uint8Array = typeof text === 'string' ? toUTF8Array(text) : new Uint8Array(text)
+  const uint8Array = typeof text === 'string' ? textEncoder.encode(text) : new Uint8Array(text)
   return nobleSha256(uint8Array)
 }
 
 export const sha384 = (text: string | ArrayBuffer): Uint8Array => {
-  const uint8Array = typeof text === 'string' ? toUTF8Array(text) : new Uint8Array(text)
+  const uint8Array = typeof text === 'string' ? textEncoder.encode(text) : new Uint8Array(text)
   return nobleSha384(uint8Array)
 }
 
 export const sha512 = (text: string | ArrayBuffer): Uint8Array => {
-  const uint8Array = typeof text === 'string' ? toUTF8Array(text) : new Uint8Array(text)
+  const uint8Array = typeof text === 'string' ? textEncoder.encode(text) : new Uint8Array(text)
   return nobleSha512(uint8Array)
 }
 
