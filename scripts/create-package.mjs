4cc07f00929e45eb1deb173ea8a960891e53c6a7
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2026-03-23 12:17:38 +0100

    fix: make all new packages public
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

--- scripts/create-package.mjs
+++ scripts/create-package.mjs
@@ -10,6 +10,7 @@
  *   node scripts/create-package.mjs eudi-lote --eudi
  */
 
+import { access } from 'node:fs'
 import { mkdir, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 
