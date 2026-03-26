dd6b0d19790f3110890f2b9bc9389e425a29aa5d
Berend Sliedrecht <berend@animo.id>
2026-03-26 12:20:23 +0100

    feat(sca): issuer metadata parsing
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- packages/eudi-sca/src/__tests__/utils.ts
+++
@@ -1,8 +0,0 @@
-import { hash as nodeHasher, randomBytes } from 'node:crypto'
-
-export const hasher = (toBeHashed: string) => ({
-  algorithm: 'sha256',
-  hash: nodeHasher('sha256', toBeHashed),
-})
-
-export const jtiGenerator = () => randomBytes(16).toString('base64')
--- packages/eudi-sca/src/error.ts
+++
@@ -1,6 +0,0 @@
-export class ScaError extends Error {
-  constructor(message = new.target.name) {
-    super()
-    super.message = message
-  }
-}
